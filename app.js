dbbd85494804f5409d25ef846879361e1ce19e2b
vlmna13 <vlmna13@gmail.com>
2024-12-11 00:07:11 +0300

    refactor: update HTML and JS according to the rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ var createNewTaskElement=function(taskString){
     //button.delete
     var deleteButton=document.createElement('button');//delete button
     var deleteButtonImg=document.createElement('img');//delete button image
-
+    deleteButtonImg.alt = 'remove';
     label.innerText=taskString;
     label.className='task';
 
